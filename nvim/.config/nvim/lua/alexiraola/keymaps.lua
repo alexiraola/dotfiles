1bd9e2126f398033ba11ba30fa1c1b400742fe51
Alex Iraola <alex.iraola@gmail.com>
2022-10-14 15:49:47 +0200

    Useful terminal maps.

--- nvim/.config/nvim/lua/alexiraola/keymaps.lua
+++ nvim/.config/nvim/lua/alexiraola/keymaps.lua
@@ -9,3 +9,14 @@ vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'kj', '<Esc>')
+
+-- Terminal maps
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set('t', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('t', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('t', '<C-l>', '<C-\\><C-N><C-w>l')
+vim.keymap.set('i', '<C-h>', '<C-\\><C-N><C-w>h')
+vim.keymap.set('i', '<C-j>', '<C-\\><C-N><C-w>j')
+vim.keymap.set('i', '<C-k>', '<C-\\><C-N><C-w>k')
+vim.keymap.set('i', '<C-l>', '<C-\\><C-N><C-w>l')
