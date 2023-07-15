732fc1b94a6f15922580bee5e9498b30416bddd4
Alex Iraola <alex.iraola@gmail.com>
2023-07-15 15:43:26 +0200

    More fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+eval (brew shellenv)
+
+set -U fish_greeting # disable fish greeting
+set -U fish_key_bindings fish_vi_key_bindings
+
+set -Ux EDITOR nvim
+
 set -gx CDPATH $CDPATH . ~ ~/Projects
 set -gx GPG_TTY $TTY
 
