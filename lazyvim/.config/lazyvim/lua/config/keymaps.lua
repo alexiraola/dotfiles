890f37e96d50e34bc2cb4b1687c6268095c36d32
Alex Iraola <alex.iraola@gmail.com>
2024-01-08 09:58:32 +0100

    Lazyvim config changes

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<c-j>", ":TmuxNavigateDown<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<c-k>", ":TmuxNavigateUp<cr>", { silent = true, noremap = true })
+
+vim.keymap.del("n", "s")
---
+++ lazyvim/.config/lazyvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_hidden = false,
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+}
