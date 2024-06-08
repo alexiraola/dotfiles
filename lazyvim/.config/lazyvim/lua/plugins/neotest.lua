c7355a17aa404bb9cd0c7c7ba4141a23007bb113
Alex Iraola <alex.iraola@gmail.com>
2024-06-08 16:31:34 +0200

    Add vitest to neotest, update lazyvim

--- lazyvim/.config/lazyvim/lua/plugins/neotest.lua
+++ lazyvim/.config/lazyvim/lua/plugins/neotest.lua
@@ -2,8 +2,9 @@ return {
   { "nvim-neotest/neotest-plenary" },
   { "nvim-neotest/neotest-go" },
   { "haydenmeade/neotest-jest" },
+  { "marilari88/neotest-vitest" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary", "neotest-jest", "neotest-go", "rustaceanvim.neotest" } },
+    opts = { adapters = { "neotest-plenary", "neotest-jest", "neotest-vitest", "neotest-go", "rustaceanvim.neotest" } },
   },
 }
