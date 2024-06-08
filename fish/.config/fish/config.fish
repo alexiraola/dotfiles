fffbfe573f4a470e0e8aa99d0a6b699dbe9f3664
Alex Iraola <alex.iraola@gmail.com>
2024-06-08 16:31:06 +0200

    Remove NVM, add mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,6 +20,8 @@ fish_add_path $HOME/.config/bin
 fish_add_path /usr/local/sbin
 fish_add_path $HOME/development/flutter/bin
 fish_add_path $HOME/android-sdk-macosx/platform-tools
+fish_add_path $HOME/.deno/bin
+fish_add_path $HOME/.local/bin
 
 alias vim="NVIM_APPNAME=lazyvim nvim"
 alias nvim-lazyvim="NVIM_APPNAME=lazyvim nvim"
@@ -28,3 +30,8 @@ alias bat="batcat"
 
 zoxide init --cmd cd fish | source
 starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+/home/alex/.local/bin/mise activate fish | source
